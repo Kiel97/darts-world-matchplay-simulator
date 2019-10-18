66de8a03e98683de5b492c40c10656b048c146ba
Kiel <kkielek97@gmail.com>
2019-10-18 13:49:10 +0200

    Implement extended matches when 2 players have objective-1 score to 2 clear legs

--- match.py
+++ match.py
@@ -12,6 +12,9 @@ class Match():
         self.player1_legs = 0
         self.player2_legs = 0
 
+        self.extended = False
+        self.max_legs = legs_to_win + 3
+
     def __repr__(self):
         return f"{self.player1} {self.player1_legs}:{self.player2_legs} {self.player2}"
 
@@ -21,13 +24,22 @@ class Match():
         else:
             self.player2_legs += 1
 
+        if not self.extended and self.player1_legs == self.legs_to_win-1 and self.player2_legs == self.legs_to_win-1:
+            self.extended = True
+
         self._check_if_finished()
 
     def _check_if_finished(self):
-        if self.player1_legs == self.legs_to_win:
-            self.winner = self.player1
-        elif self.player2_legs == self.legs_to_win:
-            self.winner = self.player2
+        if not self.extended:
+            if self.player1_legs == self.legs_to_win:
+                self.winner = self.player1
+            elif self.player2_legs == self.legs_to_win:
+                self.winner = self.player2
+        else:
+            if self.player1_legs == self.player2_legs+2 or self.player1_legs == self.max_legs:
+                self.winner = self.player1
+            elif self.player2_legs == self.player1_legs+2 or self.player2_legs == self.max_legs:
+                self.winner = self.player2
 
     def simulate(self):
         while not isinstance(self.winner, Player):
